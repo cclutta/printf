6b833c6634c39acc0b896511c353e82be097084f
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-19 19:51:21 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,6 +7,11 @@ int get_format(va_list arg, const char *string);
 int check_percent(int *f, char a);
 int _function_manager(char a, va_list arg);
 int _switch(char c, va_list arg);
-int function_manager(char c, va_list arg)
+int function_manager(char c, va_list arg);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *str);
+char *convert_to(char representation[], unsigned int num, int base);
+
 
 
