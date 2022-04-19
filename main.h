8d93fb223f592f6b0b1e5fe9a46e203678e42ad5
cclutta <52520502+cclutta@users.noreply.github.com>
2022-04-19 19:56:16 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _printf(const *char format, ...);
+int _printf(const char *format, ...);
 int get_format(va_list arg, const char *string);
 int check_percent(int *f, char a);
 int _function_manager(char a, va_list arg);
